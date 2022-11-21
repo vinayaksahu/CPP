2f328fd238ff2a47ebcb26df4318944ff8eb2404
Vinayak Sahu <vinayaksahu3@gmail.com>
2022-11-21 23:51:55 +0530

    Create Input_any_integer.cpp

---
+++ Input_any_integer.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Input any integer and then print
+    int num;
+    cout << "Input any integer number: ";
+    cin >> num;
+    cout << num<<endl;
+
+    return 0;
+}
